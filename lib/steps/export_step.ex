9b3d1543e55eb8e974559148865c59c8611776c6
Данил Путинцев <workdanilput@mail.ru>
2025-12-15 23:09:09 +0300

    save json

--- lib/steps/export_step.ex
+++ lib/steps/export_step.ex
@@ -1,5 +1,6 @@
 defmodule FpLab4.Steps.ExportStep do
   alias Workflows.Interpolator
+  require Logger
 
   @doc """
   Экспортирует данные в различные форматы
@@ -7,6 +8,8 @@ defmodule FpLab4.Steps.ExportStep do
   def save_json(params, context) do
     data = get_input(params["data"], context)
     filename = Interpolator.interpolate(params["filename"], context)
+
+    Logger.debug("Data type before encoding: #{inspect(data |> Map.get(:__struct__) || "plain")}")
 
     json_data = Jason.encode!(data, pretty: true)
 
@@ -51,9 +54,26 @@ defmodule FpLab4.Steps.ExportStep do
     end
   end
 
+  # Обработка ссылок на контекст в формате {{key}}
   defp get_input("{{" <> rest, context) do
     key = String.trim_trailing(rest, "}}") |> String.to_atom()
     Map.get(context, key)
   end
+
+  # Обработка уже сериализованных Elixir-термов (строк, начинающихся с %{)
+  defp get_input(<<first::binary-size(2)>> <> _ = value, context) when first in ["%{", "%["] do
+    try do
+      # Пытаемся распарсить Elixir-терм из строки
+      {result, _bindings} = Code.eval_string(value)
+      result
+    rescue
+      e ->
+        Logger.error("Failed to parse Elixir term: #{inspect(e)}")
+        # Если не удалось распарсить, возвращаем как есть
+        value
+    end
+  end
+
+  # Обработка простых значений
   defp get_input(value, _context), do: value
 end
